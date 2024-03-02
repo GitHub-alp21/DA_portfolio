33d9ad9eb81d615f8fffdd5f2035e91b33819409
alp21 <you@example.com>
2024-03-02 14:49:01 +0700

    first commit

---
+++ returnrate_by_products.sql
@@ -0,0 +1,47 @@
+﻿-- Analyze return rates by product
+-----------------------------------------------------
+--CTE
+WITH CTE
+AS (
+   --
+   SELECT a.ProductKey,
+          a.order_quantity,
+          b.return_quantity
+   FROM
+   (
+       -- Combine 3 sales tables with the same structure, then calculate the total order quantity by product key
+       SELECT sales.productkey,
+              SUM(sales.orderquantity) order_quantity
+       FROM
+       (
+           SELECT *
+           FROM sales2015
+           UNION
+           SELECT *
+           FROM sales2016
+           UNION
+           SELECT *
+           FROM sales2017
+       ) AS sales
+       GROUP BY sales.ProductKey
+   ) a
+       -- join with returns table
+       LEFT JOIN
+       -- Total number of returns by product key in 3 years
+       (
+           SELECT r.ProductKey,
+                  SUM(r.returnquantity) return_quantity
+           FROM returns r
+           GROUP BY r.ProductKey
+       ) b
+           ON a.ProductKey = b.ProductKey)
+-- join with products table
+-- Calculate the return rate
+SELECT CTE.ProductKey,
+       p.ProductSKU,
+       CTE.order_quantity,
+       CTE.return_quantity,
+       (CTE.return_quantity / CTE.order_quantity) AS return_rate
+FROM CTE
+    JOIN products p
+        ON CTE.ProductKey = p.ProductKey;
---
+++ returnrate_by_territory.sql
@@ -0,0 +1,47 @@
+﻿-- Analyze return rates by territory
+-----------------------------------------------------
+--CTE
+WITH CTE
+AS (
+   --
+   SELECT a.TerritoryKey,
+          a.order_quantity,
+          b.return_quantity
+   FROM
+   (
+       -- Combine 3 sales tables with the same structure, then calculate the total order quantity by territory key
+       SELECT sales.TerritoryKey,
+              SUM(sales.orderquantity) order_quantity
+       FROM
+       (
+           SELECT *
+           FROM sales2015
+           UNION
+           SELECT *
+           FROM sales2016
+           UNION
+           SELECT *
+           FROM sales2017
+       ) AS sales
+       GROUP BY sales.TerritoryKey
+   ) a
+       -- join with returns table
+       LEFT JOIN
+       -- Total number of returns by territory key in 3 years
+       (
+           SELECT r.TerritoryKey,
+                  SUM(r.returnquantity) return_quantity
+           FROM returns r
+           GROUP BY r.TerritoryKey
+       ) b
+           ON a.TerritoryKey = b.TerritoryKey)
+--join Territories table
+SELECT CTE.TerritoryKey,
+       t.Region,
+       t.Country,
+       CTE.order_quantity,
+       CTE.return_quantity,
+       (CTE.return_quantity / CTE.order_quantity) return_rate
+FROM CTE
+    LEFT JOIN Territories t
+        ON CTE.TerritoryKey = t.SalesTerritoryKey;
